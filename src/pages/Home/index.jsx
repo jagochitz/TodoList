b3d13159feb137b2a7c258e02478dfd293a2d19f
Rafael Bertoldo <jagoxitz@gmail.com>
2021-07-24 16:28:15 -0300

    feat: landing page completed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,34 @@
-function Home() {
-  return <div>Home</div>;
+import { Container, Content } from "./styles";
+import Button from "../../components/Button";
+import { Redirect, useHistory } from "react-router-dom";
+
+function Home({ authenticated }) {
+  const history = useHistory();
+
+  const handleNavigation = (path) => {
+    return history.push(path);
+  };
+
+  if (authenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <Container>
+      <Content>
+        <h1>
+          animes do<span>.</span>it
+        </h1>
+        <span>Organize suas tarefas de forma ágil e descomplicada</span>
+        <div>
+          <Button onClick={() => handleNavigation("/signup")} whiteSchema>
+            Cadastre-se
+          </Button>
+          <Button onClick={() => handleNavigation("/login")}>Login</Button>
+        </div>
+      </Content>
+    </Container>
+  );
 }
 
 export default Home;
